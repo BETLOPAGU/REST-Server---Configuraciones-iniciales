c2067172c8e1e5f1ac4442f690fed716530d7a7a
Nadia <nbla.escom@gmail.com>
2021-09-17 10:24:16 -0500

    file uploaded added

---
+++ routes/uploads.js
@@ -0,0 +1,29 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validarCampos, validarArchivoSubir } = require('../middlewares');
+const { cargarArchivos, 
+        actualizarImagen, 
+        mostrarImagen,
+        actualizarImagenCloudinary } = require('../controllers/uploads');
+const { coleccionesPermitidas } = require('../helpers');
+
+
+
+const router = Router();
+
+router.post('/', cargarArchivos);
+router.put('/:coleccion/:id', [
+    validarArchivoSubir,
+    check('id', 'El id debe de ser de mongo').isMongoId(),
+    check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
+    validarCampos
+], actualizarImagenCloudinary)
+// actualizarImagen)
+router.get('/:coleccion/:id', [
+    check('id', 'El id debe de ser de mongo').isMongoId(),
+    check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
+    validarCampos
+], mostrarImagen)
+
+module.exports = router;
